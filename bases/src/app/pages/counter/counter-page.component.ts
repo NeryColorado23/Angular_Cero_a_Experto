b1656f905e92d2d94d10aa45c769ed5effd53fcc
NeryColorado23 <ncoloradom@miumg.edu.gt>
2026-02-16 19:23:56 -0600

    signals

--- bases/src/app/pages/counter/counter-page.component.ts
+++ bases/src/app/pages/counter/counter-page.component.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 
 @Component({
-  template:`
-    <h1>Hola Mundo {{counter}}</h1>
-    <button (click)="increaseBy(1)">+1</button>
-    <h2>Counter component page</h2>
-    `
+  //importo componente html de counter
+  templateUrl: './counter-page.component.html',
+  styles: `
+  button{
+    padding: 5px;
+    margin: 5px 10px;
+    width: 75px;
+  }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 
 //para crear componente se usa @component
 
 export class CounterPageComponent {
  counter = 10;
+
+ //señales o signals para comunicar componentes, se pueden usar para emitir eventos o datos entre componentes
+ counterSignal = signal(10);
+
+ constructor() {
+  setInterval(() => {
+    console.log('tick');
+  },2000);
+ }
+
  increaseBy(value: number){
   //use this para acceder a las propiedades de la clase counterpage de esta clase
   this.counter += value;
+  this.counterSignal.update( (current) => current + value);
  }
+  resetCounter(){
+    this.counter = 0;
+    this.counterSignal.set(0);
+  }
 };
